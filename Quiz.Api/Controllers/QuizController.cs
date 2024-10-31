ecfdc18741a2737202a9311eafbda07a60892896
andrzej-herman <aherman@san.edu.pl>
2024-10-31 09:27:43 +0100

    31.10.2024

--- Quiz.Api/Controllers/QuizController.cs
+++ Quiz.Api/Controllers/QuizController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
 
@@ -23,7 +24,22 @@ namespace Quiz.Api.Controllers
 		public string CheckAnswers([FromBody] int questionId, int answerId)
 		{
 			return $"Dobra odpowiedü";
-		}
+        }
 
 	}
 }
+
+
+
+// CONNECTION STRING
+
+// PRZYK£AD
+// "Server=myServerName\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword;"
+
+// LOKALNA INSTANCJA
+// "Server=.\\HERMANLOCAL;Database=QuizDbDzienneGr3;Integrated Security=True;"
+
+// PRZYK£ADOWY CONNECTION STRING DO BAZY NA AZURE
+// "Server=tcp:san-edu-pl.database.windows.net,1433;Initial Catalog=san;Persist Security Info=False;User ID=aherman;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+
---
+++ Quiz.QuestionsCreator/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Quiz.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
+
+var connectionString = "Server=.\\HERMANLOCAL;Database=QuizDbDzienneGr3;Integrated Security=True;";
+var path = "S:\\ZIMA 2024\\questions.json";
+var json = File.ReadAllText(path);
+
+// desarialzujemy plik json na listę pytań:
+var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+var questions = JsonSerializer.Deserialize<List<Question>>(json);
+
+
+
+var connection = new SqlConnection(connectionString);
+connection.Open();
+if (questions != null)
+{
+    var counter = 1;
+    foreach (var question in questions!)
+    {
+        var properContent = question.Content!.Replace("'", "\"");
+        string query = $"INSERT INTO Questions VALUES ({question.Category}, '{properContent}')";
+        var command = new SqlCommand(query, connection);
+        command.ExecuteNonQuery();
+        Console.WriteLine($" Dodano pytanie nr {counter}");
+        counter++;
+       
+    }
+}
+
+connection.Close();
+
+Console.ReadLine();
+
+
